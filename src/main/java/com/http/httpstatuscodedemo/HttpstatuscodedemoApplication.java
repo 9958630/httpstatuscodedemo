84cb3de289e55e2e100b5b045280ea2266bce58f
Bala Chadramouli, Ketharaju <ketharaju.balachadramouli@lowes.com>
2023-02-21 20:10:32 +0530

    My First commit for http status code

---
+++ src/main/java/com/http/httpstatuscodedemo/HttpstatuscodedemoApplication.java
@@ -0,0 +1,13 @@
+package com.http.httpstatuscodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstatuscodedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstatuscodedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/http/httpstatuscodedemo/HttpstatuscodedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.httpstatuscodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpstatuscodedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
